2f56f23a5485aaa3526ed5ae144af4a14d167743
kaakaa <stooner.hoe@gmail.com>
2014-06-23 19:47:00 +0900

    first

---
+++ src/main/java/org/sample/systemrules/SystemOutSample.java
@@ -0,0 +1,15 @@
+package org.sample.systemrules;
+
+/**
+ * Created by kaakaa_hoe on 2014/06/23.
+ */
+public class SystemOutSample {
+    public void say() {
+        System.out.println("Hi!");
+    }
+
+    public void sayMultiLine() {
+        System.out.println("Hi!");
+        System.out.println("Bye!");
+    }
+}
---
+++ src/test/java/org/sample/systemrules/SystemOutSampleTest.java
@@ -0,0 +1,64 @@
+package org.sample.systemrules;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.contrib.java.lang.system.StandardOutputStreamLog;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.*;
+
+public class SystemOutSampleTest {
+    private SystemOutSample target;
+
+    private static final String newLine = System.getProperty("line.separator");
+
+    @Before
+    public void setUp(){
+        target =new SystemOutSample();
+    }
+
+    @Rule
+    public final StandardOutputStreamLog log = new StandardOutputStreamLog();
+
+    @Test
+    public void 標準出力のテスト(){
+        // when
+        System.out.print("Hello SystemRules!");
+
+        // then
+        assertThat(log.getLog(), is("Hello SystemRules!"));
+    }
+
+    @Test
+    public void 呼び出し先メソッドの標準出力のテスト(){
+        // when
+        target.say();
+
+        // then
+        assertThat(log.getLog(), is("Hi!" + newLine));
+    }
+
+    @Test
+    public void 呼び出し先メソッドの標準出力のテスト_複数行(){
+        // when
+        target.sayMultiLine();
+
+        // then
+        assertThat(log.getLog(), is("Hi!" + newLine + "Bye!" + newLine));
+    }
+
+    @Test
+    public void 呼び出し先メソッドの標準出力のテスト_複数行_assertを分かりやすく(){
+        // when
+        target.sayMultiLine();
+
+        // then
+        List<String> actualLog = Arrays.asList(log.getLog().split(newLine));
+        assertThat(actualLog, contains("Hi!", "Bye!"));
+    }
+}
